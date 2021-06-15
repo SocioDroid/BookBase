dd9ab72a8d5e9b3422adc60a517d7ccdb416a2ab
SocioDroid <sociodroid2019@gmail.com>
2021-06-15 19:53:55 +0530

    Renamed Procfile

--- BookBase/settings.py
+++ BookBase/settings.py
@@ -126,7 +126,8 @@ MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = [STATIC_ROOT]
+# STATICFILES_DIRS = [STATIC_DIR]
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
